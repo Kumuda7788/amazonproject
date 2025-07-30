d1c811819abc9cb274a28d71eecc1bc226ac30ec
Kumuda7788 <kumudapradhan582@gmail.com>
2025-07-30 20:04:21 +0530

    this is the first commit of amazon project by test lead

---
+++ src/demopackage/testcase001.java
@@ -0,0 +1,14 @@
+package demopackage;
+
+public class testcase001 {
+	public static void main(String[] args) {
+
+		int x = 50;
+		if (x / 2 == 0) {
+			System.out.println("x is  an even number");
+		} else {
+			System.out.println("x is not an even number");
+		}
+
+	}
+}
